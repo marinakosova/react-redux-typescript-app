882a68601a52bcf78b2b67fe1fddf3018f61b52f
marinakosova <marina.kosova@gmail.com>
2021-12-01 17:30:10 -0800

    Add Slice for counter

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -3,3 +3,20 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface CounterState {
     value: number;
 }
+
+const initialState: CounterState = {
+    value: 0,
+};
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        incremented(state) {
+            state.value++;
+        }
+    }
+});
+
+export const { incremented } = counterSlice.actions;
+export default counterSlice.reducer;
